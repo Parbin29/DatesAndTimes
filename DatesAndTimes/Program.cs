e9b8eaed4b7904c9c7a1b6485abb62dc1e3c1225
Parbin29 <naazu1295@gmail.com>
2024-10-30 23:29:38 +0100

    myage calculator

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            DateTime myValue = DateTime.Now;
+            // Console.WriteLine(myValue.ToString());
+            // Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            // Console.WriteLine(myValue.ToLongTimeString());
+
+            // Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            // Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            // Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            // Console.WriteLine(myValue.Month);
+
+
+            // DateTime myBirthday = new DateTime(1995,12,31);
+            // Console.WriteLine(myBirthday.ToShortDateString());
+
+
+            DateTime myBirthday = DateTime.Parse("03/06/1995");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine("Age in days: ");
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+
+}
